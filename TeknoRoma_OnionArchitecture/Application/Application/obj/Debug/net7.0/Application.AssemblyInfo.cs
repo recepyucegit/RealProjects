8e9d45c98b7450cf7dd4f54a166e7ac6dbbadf3d
recepyucegit <recep.yuce1789@gmail.com>
2025-11-18 11:42:45 +0300

    Reset index and remove .vs traces

--- TeknoRoma_OnionArchitecture/Application/Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ TeknoRoma_OnionArchitecture/Application/Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+765031c1aa98631b69d08ecf88f7f01d53a5eb4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1114cd2e8cce1ae995a15e3b3bf1e8d486b2bbd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
